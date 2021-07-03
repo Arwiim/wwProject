e36fab38647285f4a56fb2de8a8368c79d054255
Gabi Moreno <solo.games.9@hotmail.com>
2021-07-03 12:26:25 -0300

    Feature for users change password if they forget

--- blog/settings.py
+++ blog/settings.py
@@ -135,6 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+LOGIN_URL = 'users:login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
