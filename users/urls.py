eb2406b0ceb1a86b774401250c2ea3ac4baa2625
Gabi Moreno <solo.games.9@hotmail.com>
2021-07-02 21:31:53 -0300

    Add function for send email verification when a user is register

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('edit', views.edit_profile, name='edit'),
     path('logout', views.user_logout, name='logout'),
     path('recover', views.user_recovery_password, name='recover_password'),
+    path('activate-user/<uidb64>/<token>',views.activate_user, name='activate'),
     path('recover-confirm', views.user_password_confirm, name='recover_password_confirm'),
 ]
