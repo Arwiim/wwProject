d52dabf5e3c3f1926996d3a78495e13ca539464f
Gabi Moreno <solo.games.9@hotmail.com>
2021-07-07 18:16:32 -0300

    Remove libs no longeer needed

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic.base import TemplateView
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from . import views
@@ -15,10 +13,10 @@ urlpatterns = [
     path('edit', views.edit_profile, name='edit'),
     path('logout', views.user_logout, name='logout'),
     path('activate-user/<uidb64>/<token>',views.activate_user, name='activate'),
-    # Password reset contrib from django
+    # Password change section
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='account/password_change/password_change_form.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change/password_change_done.html'), name='password_change_done'),
-
+    # Passwird reset section
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='account/password_change/password_reset_form.html',
                                                                  email_template_name='account/password_change/password_reset_email.html',
                                                                  success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
