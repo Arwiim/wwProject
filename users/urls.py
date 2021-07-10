ac48e39d713918f0d96caa967b5e1ec778794074
Gabi Moreno <solo.games.9@hotmail.com>
2021-07-09 23:59:00 -0300

    Add authentication for facebook and fix bugs in registration method

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
@@ -16,7 +16,7 @@ urlpatterns = [
     # Password change section
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='account/password_change/password_change_form.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change/password_change_done.html'), name='password_change_done'),
-    # Passwird reset section
+    # Password reset section
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='account/password_change/password_reset_form.html',
                                                                  email_template_name='account/password_change/password_reset_email.html',
                                                                  success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_change/password_reset_confirm.html',
                                                                                 success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
+    # Social auth
 ]
