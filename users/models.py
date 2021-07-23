8705957c1b73efc19846ca13ad64db014a4abf68
Gabi Moreno <solo.games.9@hotmail.com>
2021-07-22 21:22:22 -0300

    pylint

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
+"""Models for users
+"""
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
+from django.contrib.auth.models import AbstractUser
 
 class Profile(models.Model):
     """ Profile Model
@@ -11,11 +12,13 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='users/pictures', blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     date_modified = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self) -> str:
         return self.user.username
 
 class User(AbstractUser):
+    """User Model inherit from User from Django
+    """
     is_verified = models.BooleanField(default=False)
 
     def __str__(self) -> str:
